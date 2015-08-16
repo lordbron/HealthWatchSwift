4c624adc2592a5778486d981935d5985c61b1cba
mknierle <nierlemk@vcu.edu>
2015-08-16 16:14:49 -0400

    Initial Commit

---
+++ HealthWatchSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthWatchSwift
+//
+//  Created by Melissa Nierle on 8/16/15.
+//  Copyright (c) 2015 Melissa Nierle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
